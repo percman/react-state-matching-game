97716134d6bd2cd1f19c3ebc56b32348bd29ce74
Daniel Abramson <daniel.abramson@ruralsource.com>
2020-06-09 15:03:14 -0600

    Complete module 3

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -7,7 +7,11 @@ const Tile = (props) => {
     props.selected || props.matched ? { backgroundColor: props.color } : null;
 
   return (
-    <div className="Tile" style={color}>
+    <div
+      className="Tile"
+      style={color}
+      onClick={() => props.handleTileClicked(props.id, props.color)}
+    >
       {props.selected || props.matched ? <props.svg /> : null}
     </div>
   );
